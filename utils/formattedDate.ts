beafac70173850f08e2c005c307048bf80ddd3f7
olhkyle <kylekwon96@gmail.com>
2023-07-18 03:48:31 +0900

    [FIX] update formattedDate util function

--- utils/formattedDate.ts
+++ utils/formattedDate.ts
@@ -2,10 +2,10 @@ const insertStringZero = (stringifyDate: string) => stringifyDate.padStart(2, '0
 const isString = (date: Date | string) => typeof date === 'string';
 
 const formattedDate = ({ startDate, endDate }: { startDate: Date; endDate: Date | string }) => {
-	const start = `${startDate.getFullYear()}.${insertStringZero(startDate.getMonth() + '')}`;
+	const start = `${startDate.getFullYear()}.${insertStringZero(startDate.getMonth() + 1 + '')}`;
 
 	const end = `${isString(endDate) ? endDate : (endDate as Date).getFullYear()}${isString(endDate) ? '' : '.'}${
-		isString(endDate) ? '' : insertStringZero((endDate as Date).getMonth() + '')
+		isString(endDate) ? '' : insertStringZero((endDate as Date).getMonth() + 1 + '')
 	}`;
 
 	return `${start} ${isString(endDate) && endDate === '' ? '' : '-'} ${end}`;
