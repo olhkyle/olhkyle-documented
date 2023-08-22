ffb108f3fc1779c18ca2bb4388d89fc8d8a7d9c1
olhkyle <kylekwon96@gmail.com>
2023-08-23 00:13:07 +0900

    [REFAC] refactor Type on FormattedDate

--- utils/formattedDate.ts
+++ utils/formattedDate.ts
@@ -1,7 +1,11 @@
+import { Project } from '@/data/projects';
+
 const insertStringZero = (stringifyDate: string) => stringifyDate.padStart(2, '0');
 const isString = (date: Date | string) => typeof date === 'string';
 
-const formattedDate = ({ startDate, endDate }: { startDate: Date; endDate: Date | string }) => {
+type FormattedDate = Pick<Project, 'startDate' | 'endDate'>;
+
+const formattedDate = ({ startDate, endDate }: FormattedDate) => {
 	const start = `${startDate.getFullYear()}.${insertStringZero(startDate.getMonth() + 1 + '')}`;
 
 	const end = `${isString(endDate) ? endDate : (endDate as Date).getFullYear()}${isString(endDate) ? '' : '.'}${
