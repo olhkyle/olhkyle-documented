f63f8a6841f754dbc666de9ca48d67e9f40e60cd
olhkyle <kylekwon96@gmail.com>
2023-08-23 00:11:30 +0900

    [-] change MainTitle Component's Prop Type name

--- components/common/MainTitle.tsx
+++ components/common/MainTitle.tsx
@@ -1,7 +1,7 @@
-interface TitleProps {
+interface MainTitleProps {
 	children: React.ReactNode;
 }
 
-export default function Title({ children }: TitleProps) {
+export default function MainTitle({ children }: MainTitleProps) {
 	return <h2 className="mt-6 text-[57px] font-bold break-keep sm:text-8xl sm:mt-0">{children}</h2>;
 }
