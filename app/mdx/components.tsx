f09b9e0f95d6b675bbf86e63c797f01789a5ffa3
olhkyle <kylekwon96@gmail.com>
2023-08-23 00:08:11 +0900

    [CHORE] remove package related to MDX

--- app/mdx/components.tsx
+++
@@ -1,6 +0,0 @@
-export const components = {
-	h1: ({ children }: { children: React.ReactNode }) => <h1 style={{ fontSize: '45px' }}>{children}</h1>,
-	h2: ({ children }: { children: React.ReactNode }) => <h2 style={{ fontSize: '36px' }}>{children}</h2>,
-	h3: ({ children }: { children: React.ReactNode }) => <h3 style={{ fontSize: '27px' }}>{children}</h3>,
-	h4: ({ children }: { children: React.ReactNode }) => <h4 style={{ fontSize: '21px' }}>{children}</h4>,
-};
--- next.config.js
+++ next.config.js
@@ -6,13 +6,4 @@ const nextConfig = {
 	reactStrictMode: true,
 };
 
-const withMDX = require('@next/mdx')({
-	extension: /\.mdx?$/,
-	options: {
-		remarkPlugins: [],
-		rehypePlugins: [],
-		// If you use `MDXProvider`, uncomment the following line.
-		providerImportSource: '@mdx-js/react',
-	},
-});
-module.exports = withMDX(nextConfig);
+module.exports = nextConfig;
