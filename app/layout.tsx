949a4b55fce21c300e3db6e67e5333073f33634d
olhkyle <kylekwon96@gmail.com>
2023-08-29 23:11:24 +0900

    [REFAC] rollback dark mode code

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css';
 import React from 'react';
 import type { Metadata } from 'next';
 import { Footer, Nav, ScrollToTopButton } from '@/components';
+import { cookies } from 'next/dist/client/components/headers';
 import { Schibsted_Grotesk } from 'next/font/google';
 
 export const metadata: Metadata = {
@@ -21,31 +22,7 @@ const schibsted_Grotesk = Schibsted_Grotesk({
 });
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-	// Dark Mode -> CSR & SSR synchronization
-	const script = `
-			(function(){
-				const savedTheme = localStorage.getItem('theme');
-				const isDark = window.matchMedia('(prefers-color-scheme: dark)');
-
-				const theme = savedTheme ?? (isDark.matches ? 'dark' : 'light');
-
-				const setTheme = (newTheme) => {
-					if (newTheme === 'dark') {
-						document.body.dataset.theme = 'dark';
-					} else {
-						document.body.dataset.theme = 'light';
-					}
-
-					localStorage.setItem('theme', newTheme);
-				};
-
-				isDark.addListener((e) => {
-					setTheme(e.matches ? 'dark' : 'light');
-				});
-
-				setTheme(theme);
-			})();
-	`;
+	const cookie = cookies().get('theme');
 
 	return (
 		<html lang="en">
@@ -62,12 +39,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 				<meta name="theme-color" content="#fff" media="(prefers-color-scheme: light)" />
 				<meta name="theme-color" content="#090b16" media="(prefers-color-scheme: dark)" />
 			</head>
-			<body className={schibsted_Grotesk.className} suppressHydrationWarning={true}>
+			<body data-theme={cookie && cookie.value} className={schibsted_Grotesk.className}>
 				<Nav />
 				<main className="mx-auto px-[2rem] h-full sm:w-[640px] md:w-[768px] lg:w-[1024px]">{children}</main>
 				<Footer />
 				<ScrollToTopButton topPosToStartShowing={300} />
-				<script dangerouslySetInnerHTML={{ __html: script }} />
 			</body>
 		</html>
 	);
